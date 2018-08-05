a2eeb6d07b29ea8143f52441c69a6e9ee4153cb3
Matt Beyl <matthew.beyl@gmail.com>
2018-08-05 17:07:12 -0500

    OMG IT WORKS - added to pool.js and deleted pre-existing router.post

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,41 +1,61 @@
-/**
-* You'll need to use environment variables in order to deploy your
-* pg-pool configuration to Heroku.
-* It will look something like this:
-**/
+// /**
+// * You'll need to use environment variables in order to deploy your
+// * pg-pool configuration to Heroku.
+// * It will look something like this:
+// **/
+
+// const pg = require('pg');
+// const url = require('url');
+// let config = {};
+
+// if (process.env.DATABASE_URL) {
+//     // Heroku gives a url, not a connection object
+//     // https://github.com/brianc/node-pg-pool
+//     let params = url.parse(process.env.DATABASE_URL);
+//     let auth = params.auth.split(':');
+
+//     config = {
+//         user: auth[0],
+//         password: auth[1],
+//         host: params.hostname,
+//         port: params.port,
+//         database: params.pathname.split('/')[1],
+//         ssl: true, // heroku requires ssl to be true
+//         max: 10, // max number of clients in the pool
+//         idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+//     };
+
+// } else {
+//     // only change the things on the right side of the ||
+//     config = {
+//         user: process.env.PG_USER || null, //env var: PGUSER
+//         password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+//         host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+//         port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
+//         database: process.env.DATABASE_NAME || 'reflection_board', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
+//         max: 10, // max number of clients in the pool
+//         idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+//     };
+// }
+
+// module.exports = new pg.Pool(config);
 
 const pg = require('pg');
-const url = require('url');
-let config = {};
+const Pool = pg.Pool;
+const pool = new Pool({
+  host: 'localhost',
+  port: '5432',
+  database: 'prime_feedback',
+  max: 10,
+  idleTimeoutMillis: 30000
+})
 
-if (process.env.DATABASE_URL) {
-    // Heroku gives a url, not a connection object
-    // https://github.com/brianc/node-pg-pool
-    let params = url.parse(process.env.DATABASE_URL);
-    let auth = params.auth.split(':');
+pool.on('connect', () => {
+  console.log('PG connected to Posgresql');
+});
 
-    config = {
-        user: auth[0],
-        password: auth[1],
-        host: params.hostname,
-        port: params.port,
-        database: params.pathname.split('/')[1],
-        ssl: true, // heroku requires ssl to be true
-        max: 10, // max number of clients in the pool
-        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-    };
+pool.on('error', (error) => {
+  console.log('Error', error);
+});
 
-} else {
-    // only change the things on the right side of the ||
-    config = {
-        user: process.env.PG_USER || null, //env var: PGUSER
-        password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
-        host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
-        port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
-        database: process.env.DATABASE_NAME || 'reflection_board', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
-        max: 10, // max number of clients in the pool
-        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-    };
-}
-
-module.exports = new pg.Pool(config);
+module.exports = pool;
--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,11 +2,18 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-let feedback = []
+// let feedback = []
 
 router.post('/', (req, res) => {
-console.log(req.body);
-res.sendStatus(201);
-})
+    pool.query(`INSERT INTO "feedback"
+    ("feeling", "understanding", "support", "comments")
+VALUES ($1, $2, $3, $4);`, [req.body.feedback.feeling, req.body.feedback.understanding,
+        req.body.feedback.support, req.body.feedback.comments])
+        .then((results) => {
+            res.sendStatus(201);
+        }).catch((errorFromPG) => {
+            res.sendStatus(500)
+        })
+});
 
 module.exports = router;
